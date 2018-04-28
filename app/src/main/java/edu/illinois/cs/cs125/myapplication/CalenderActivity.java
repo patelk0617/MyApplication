be989ba02c4a1bb3ed0ca067b92e7102f776f746
kpia1 <kp5@illinois.edu>
2018-04-28 17:23:01 -0500

    button1done

--- app/src/main/java/edu/illinois/cs/cs125/myapplication/CalenderActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/myapplication/CalenderActivity.java
@@ -1,8 +1,10 @@
 package edu.illinois.cs.cs125.myapplication;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.TextView;
@@ -22,16 +24,20 @@ public class CalenderActivity extends AppCompatActivity {
         reminder = (Button) findViewById(R.id.reminder);
         toDo = (Button) findViewById(R.id.toDo);
         kookyMode = (Button) findViewById(R.id.kookyMode);
-//
-//        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener()) {
-//            @Override
-//            public void onSelectDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
-//                String date = (i1 + 1) + "/" + i2 + "/" + i;
-//            }
-//        });
-//
-//    }
+
+        reminder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openReminder();
+            }
+        });
+
     }
+    public void openReminder() {
+        Intent intent = new Intent(this, ReminderActivity.class);
+        startActivity(intent);
+    }
+
 }
 
 
